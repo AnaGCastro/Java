1446ec8aef4453cc0c04cff091cda09da5f4df36
AnaGCastro <61274440+AnaGCastro@users.noreply.github.com>
2022-03-20 12:34:29 -0300

    Quinto Commit
    Projetinho com base em armazenar uma medida de raio em metros, determinar e exibir o perímetro e a área de um circulo com esse raio.
    A Linguagem utilizada foi Java.

---
+++ P1E06.java
@@ -0,0 +1,18 @@
+
+public class P1E06 {
+
+	public static void main(String[] args) {
+		
+		double raio, perimetro, area;
+		
+        raio = 6.3;
+
+        perimetro = 2 * 3.14159 * raio;
+        area = 3.14159 * raio * raio;
+
+        System.out.println("O perímetro do círculo é: " + perimetro);
+        System.out.println("Areá do círculo é: " + area);
+
+	}
+
+}
